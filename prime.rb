f67ccb984d3853283c6e5dddb40bf8506b4bba34
Winter LaMon <winter.lamon@gmail.com>
2017-09-28 23:56:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,15 @@
 def prime?(num)
+  array_start = 2
+  num_array = [array_start...num]
+  
   if num < 2
     return false
   end
-  num_array = [2...num]
-    for n in num_array
-      if num % i == 0
-        false
-      else
-        true
-      end
+  
+  num.each do |n|
+    if num % n == 0
+      return false
     end
+  end
+  return true
 end
