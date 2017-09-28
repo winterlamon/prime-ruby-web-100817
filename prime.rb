4beecb47e9ea96d2057f40adc9d9f94c5ac17d8f
Winter LaMon <winter.lamon@gmail.com>
2017-09-28 23:44:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
-def prime?(integer)
-  integer_arr = [1..integer]
-    integer_arr.each do |i|
-      if integer % i == 0
+def prime?(num)
+  if num < 2
+    return false
+  num_array = [*...num]
+    for n in num_array
+      if num % i == 0
         false
       else
         true
       end
   end
 end
-# (integer % i = 0) && (i != 1) &&
-prime?(16)
