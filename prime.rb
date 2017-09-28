881ffa594b1a69c5bcb983cfa23bf76b408a363e
Winter LaMon <winter.lamon@gmail.com>
2017-09-28 23:26:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+  integer_arr = [1..integer]
+    integer_arr.each do |i|
+      if integer % i == 0
+        false
+      else 
+        true
+      end
+  end
+end
+# (integer % i = 0) && (i != 1) &&
+prime?(16)
