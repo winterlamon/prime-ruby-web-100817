dce1d5c3200b91d30ee922c377e0a3c42fb07e71
Winter LaMon <winter.lamon@gmail.com>
2017-09-29 00:04:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,11 @@
 def prime?(num)
   range_start = 2
   num_array = *(range_start...num)
-
   if num < 2
     return false
   end
-
   num_array.each do |n|
-    if num % n == 0 || num < 2 || num < 0
+    if num % n == 0
       return false
     else
       return true
