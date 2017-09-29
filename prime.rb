c7cfc4ce6e04eff4ee80d5fee1012069d9e94b56
Winter LaMon <winter.lamon@gmail.com>
2017-09-29 00:09:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  range_start = 2
+  num_array = *(range_start...num)
+  if num < 2
+    return false
+  end
+  num_array.each do |x|
+    if num % x == 0
+      return false
+    end
+  end
+  return true
+end
