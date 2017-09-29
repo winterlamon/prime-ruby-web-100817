4565bd205d4ab5b5af8fc15b33d3597baf4ad519
Winter LaMon <winter.lamon@gmail.com>
2017-09-29 00:08:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,11 +4,10 @@ def prime?(num)
   if num < 2
     return false
   end
-  num_array.each do |n|
-    if num % n == 0
+  num_array.each do |x|
+    if num % x == 0
       return false
-    else
-      return true
     end
   end
+  return true
 end
